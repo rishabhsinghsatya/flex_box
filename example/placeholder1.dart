abd5360be93630e181421953ee48b276ef64b3e3
Rishabh Singh <vineetsatya36@gmail.com>
2024-05-26 01:01:51 +0530

    Add dartdoc comments to public API elements

--- example/placeholder1.dart
+++ example/placeholder1.dart
@@ -1,1 +1,16 @@
  
+/// Constructs a [FlexBox] widget with the specified [width] and [height].
+///
+/// The [color] parameter allows you to set the background color of the container,
+/// and the [child] parameter specifies the widget to be placed inside the container.
+///
+/// Example:
+///
+/// ```dart
+/// FlexBox(
+///   width: 100,
+///   height: 100,
+///   color: Colors.blue,
+///   child: MyWidget(),
+/// )
+/// ```
